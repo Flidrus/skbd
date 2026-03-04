112c16fbffb0616fb552021f8eff28220262bb5b
Flidrus <145051041+Flidrus@users.noreply.github.com>
2026-03-04 20:39:23 +0200

    Add files via upload

--- lab3/agregat_func.sql
+++ lab3/agregat_func.sql
@@ -1,1 +1,14 @@
+-- 1. Загальна кількість подій у базі
+SELECT COUNT(*) AS total_events FROM events;
 
+-- 2. Загальна сума виручки від проданих квитків
+SELECT SUM(price) AS total_revenue FROM tickets WHERE status = 'paid';
+
+-- 3. Середня базова ціна квитка на всі події
+SELECT AVG(base_price) AS average_event_price FROM events;
+
+-- 4. Мінімальна ціна квитка серед усіх доступних подій
+SELECT MIN(base_price) AS cheapest_event FROM events;
+
+-- 5. Дата останнього проданого квитка
+SELECT MAX(purchase_date) AS last_ticket_sale FROM tickets;
